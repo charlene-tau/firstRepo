307374295eb3b3015fc8f059522307a5c199000b
charlene tau express <charlene.tan@tauexpress.com>
2024-01-02 16:34:25 +0800

    first commit to test git push

---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>My To Do List</h1>
+        <input type="text" v-model="newTodo" placeholder="Enter your task" @keyup.enter="addTodo">
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            newTodo:'',
+            todos:[]
+        }
+    },
+    methods:{
+        addTodo(){
+console.log("enter key pressed")
+console.log(this.newTodo)
+        }
+    }
+}
+</script>
+<styles>
+</styles>
